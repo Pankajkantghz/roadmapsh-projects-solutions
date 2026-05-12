89393684cb1fc6fd01c16b5fffd979d30f8858ba
Pankaj Yadav <pankaj.py96810@gmail.com>
2026-05-12 19:07:06 +0530

    added

--- frontend-projects/22. Quiz App/src/components/QuizStart.jsx
+++ frontend-projects/22. Quiz App/src/components/QuizStart.jsx
@@ -1,18 +1,14 @@
 const QuizStart = ({ onStart, totalQuestions }) => {
   return (
     <div className="w-full max-w-xl rounded-3xl bg-white p-8 shadow-2xl">
-      <h1 className="mb-4 text-center text-4xl font-bold">
-        Quiz App
-      </h1>
+      <h1 className="mb-4 text-center text-4xl font-bold">Quiz App</h1>
 
       <p className="mb-8 text-center text-gray-600">
         Test your frontend knowledge with timed questions.
       </p>
 
       <div className="mb-8 rounded-2xl bg-gray-100 p-5">
-        <h2 className="mb-4 text-lg font-semibold">
-          Quiz Details
-        </h2>
+        <h2 className="mb-4 text-lg font-semibold">Quiz Details</h2>
 
         <ul className="space-y-2 text-gray-700">
           <li>{totalQuestions} Questions</li>
