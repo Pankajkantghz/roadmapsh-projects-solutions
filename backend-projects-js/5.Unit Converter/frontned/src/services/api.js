0867d2bd21f40ac44c9d79ad925de9cd393fc27b
Pankaj Yadav <pankajkantnitian@gmail.com>
2026-05-19 16:24:32 +0530

    update production api

--- backend-projects-js/5.Unit Converter/frontned/src/services/api.js
+++ backend-projects-js/5.Unit Converter/frontned/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://unit-converter-api-ds4g.onrender.com",
+  baseURL:
+    "https://unit-converter-api-ds4g.onrender.com/api",
 });
 
 export default API;
